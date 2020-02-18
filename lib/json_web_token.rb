66b3c8eb79b0f064314f82dd5402230d096209b4
br-to <barabara0224@gmail.com>
2020-02-18 13:36:49 +0900

    トークンの期限の追加

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,5 +1,6 @@
 class JsonWebToken
-  def self.encode(payload)
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
     JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 end

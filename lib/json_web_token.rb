b503f0e9808b3fc1bcdd83494608815bcfac7641
br-to <barabara0224@gmail.com>
2020-02-18 22:21:10 +0900

    iatとissの取得

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,6 +1,8 @@
 class JsonWebToken
-  def self.encode(payload, exp = 24.hours.from_now)
+  def self.encode(payload, exp = 24.hours.from_now, iat = Time.now, iss = 'LabTaskOauth')
     payload[:exp] = exp.to_i
+    payload[:iat] = iat.to_i
+    payload[:iss] = iss
     JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 end
